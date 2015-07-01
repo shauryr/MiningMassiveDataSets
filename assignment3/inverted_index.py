960a56cde3843a8f5489c48a704ef2b09b475a85
shauryr <shauryr@gmail.com>
2015-07-01 18:32:31 +0530

    commit

---
+++ assignment3/inverted_index.py
@@ -0,0 +1,32 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[1]
+    value = record[0]
+    words = key.split()
+    for w in words:
+      mr.emit_intermediate(w, value)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+
+    mr.emit((key, list(set(list_of_values))))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
---
+++ assignment3/unique_trims.py
@@ -0,0 +1,32 @@
+__author__ = 'shaurya'
+
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    mr.emit_intermediate(value[0:-10],value[0:-10])
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+        mr.emit(key)
+
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
